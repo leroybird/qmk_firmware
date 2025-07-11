ef0ca8e2b225312bce66d2e52e7b0e2bca4ab218
Leroy Bird <leroybird1@gmail.com>
2025-07-12 11:19:55 +1200

    fixed compile

--- keyboards/keychron/common/common.mk
+++ keyboards/keychron/common/common.mk
@@ -2,5 +2,6 @@ COMMON_DIR = common
 SRC += $(COMMON_DIR)/matrix.c
 
 VPATH += $(TOP_DIR)/keyboards/keychron/$(COMMON_DIR)
+VPATH += $(TOP_DIR)/keyboards/keychron/common/language
 
 include $(TOP_DIR)/keyboards/keychron/$(COMMON_DIR)/debounce/debounce.mk
